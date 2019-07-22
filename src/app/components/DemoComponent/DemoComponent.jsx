8a90731e3ebdc955e5049b823fa554b57e073eef
Karthik <karthik.r.ramanathan@ril.com>
2019-07-22 22:23:01 +0530

    added select for type

--- src/app/components/DemoComponent/DemoComponent.jsx
+++ src/app/components/DemoComponent/DemoComponent.jsx
@@ -1,5 +1,4 @@
-import Button from "../../atomics/Button/Button";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 require("./DemoComponent.less")
 
@@ -7,14 +6,15 @@ import DATA_SAMPLE1 from "./sample1.json";
 import DATA_SAMPLE2 from "./sample2.json";
 
 
-import ChartLoader, {ChartTypes} from "../ChartLoader/ChartLoader";
+import ChartLoader, { ChartTypes } from "../ChartLoader/ChartLoader";
 // a != "Invalid Date"
 
-import {Select} from "antd";
-import 'antd/dist/antd.css';
+import { Select, Button } from "antd";
 
-export default class DemoComponent extends Component{
-	constructor(props){
+import 'antd/es/select/style';
+
+export default class DemoComponent extends Component {
+	constructor(props) {
 		super(props);
 		this.state = {
 			type: "",
@@ -22,30 +22,50 @@ export default class DemoComponent extends Component{
 			xAxis: null,
 			yAxis: null,
 			editMode: true
-		}
+		};
+		this.isValid = this.isValid.bind(this)
 	}
-	render(){
+
+
+	isValid(){
+		if(this.state.type !== null) {
+			return true
+		}
+		return false
+	}
+
+	render() {
+		let chartTypes = [];
+		for(var type in ChartTypes){
+			chartTypes.push(
+			<Select.Option value={ChartTypes[type]}>{ChartTypes[type].toLowerCase()} chart</Select.Option>
+			)
+		}
 		return (
 			<div className="DemoComponent">
 				{this.state.editMode &&
 					<div>
 						<div>
-							<Select style={{width: 300}} onChange={(value)=>{
+							<Select style={{ width: 300 }} value={this.state.type} onChange={(value) => {
+								this.state.type = value;
 								this.setState({
-									type: value,
-									editMode: false
+									editMode: this.isValid()
 								})
 							}}>
-								<Select.Option value="LINE">Line Chart</Select.Option>
-								<Select.Option value="BAR">Bar Chart</Select.Option>
-								<Select.Option value="AREA">Area Chart</Select.Option>
-								<Select.Option value="SCATTER">Scatter Chart</Select.Option>
+								{chartTypes}
 								</Select>
 						</div>
 					</div>
 				}
 				{!this.state.editMode &&
-				<ChartLoader title="icaRtt Report Graph" type={this.state.type} data={DATA_SAMPLE1.icaRttReport.items} xAxis={"dateTime"} yAxis={["excellent","acceptable","poor"]} />
+					<>
+					<Button onClick={()=>{
+						this.setState({
+							editMode: true
+						})
+					}}>Edit</Button>
+					<ChartLoader title="icaRtt Report Graph" type={this.state.type} data={DATA_SAMPLE1.icaRttReport.items} xAxis={"dateTime"} yAxis={["excellent", "acceptable", "poor"]} />
+					</>
 				}
 			</div>
 		)
