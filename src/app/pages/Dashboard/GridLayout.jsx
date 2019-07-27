6084ee0aca86f48b87fdc8ac2d9ecbb8ea99e9fd
priyama <priya.maithani@citrix.com>
2019-07-27 14:02:06 +0530

    adding config to grid blocks

--- src/app/pages/Dashboard/GridLayout.jsx
+++ src/app/pages/Dashboard/GridLayout.jsx
@@ -12,9 +12,9 @@ render() {
 
   return (
     <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-        <div key="a" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
-        <div key="b" data-grid={{i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4, static: true}}><StackItem /></div>
-        <div key="c" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2, static: true}}>c</div>
+        <div key="a" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2, static: true}}><StackItem data="usersession" type="AREA" xAxis="dateTime" yAxis="usersSessions"/></div>
+        <div key="b" data-grid={{i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}><StackItem data="usersession" type="LINE" xAxis="dateTime" yAxis="uniqueUsers"/></div>
+        <div key="c" data-grid={{i: 'a', x: 0, y: 0, w: 1, h: 2, static: true}}><StackItem data="usersession" type="BAR" xAxis="dateTime" yAxis="usersSessions"/></div>
       </ReactGridLayout>
   )
 }
